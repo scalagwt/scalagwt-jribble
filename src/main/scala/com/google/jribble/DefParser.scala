ce705d5e36a779b1602798e9ce403027dfe40705
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2010-11-13 15:52:59 +0100

    DefParser returns either DeclaredType or String with error msg.
    Instead of throwing an exception when input cannot be parsed we return
    Right(msg) describing the problem.
    
    Change-Id: Ib21e8a8ae472fbac981e21cdf0823757c2039812
    Signed-off-by: Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>

--- src/main/scala/com/google/jribble/DefParser.scala
+++ src/main/scala/com/google/jribble/DefParser.scala
@@ -25,9 +25,9 @@ class DefParser {
   }
   import parsers._
 
-  def parse(in: java.io.Reader): ast.DeclaredType = parsers.parse(phrase(defParser), in) match {
-    case Success(result, _) => result
-    case x => error("Could not parse the input " + x)
+  def parse(in: java.io.Reader): Either[ast.DeclaredType, String] = parsers.parse(phrase(defParser), in) match {
+    case Success(x, _) => Left(x)
+    case x => Right("Could not parse the input " + x)
   }
 
 }
